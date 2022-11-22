aa814185d542374472190d00973a742f9ed5ea0c
Songyue-yoyo <songyue.zhang@student.maastrichtuniversity.nl>
2022-11-22 11:39:14 +0100

    add tokenUrlApi

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x00a438FD5f0909514AA348bcdbD54198E5f7d13c";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-iota-six.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
